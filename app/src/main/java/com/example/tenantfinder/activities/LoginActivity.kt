bcb2bebacc801ad415d19439e51aa1474e4a2377
Mikejo14 <michaeljonesjmj4@gmail.com>
2022-05-13 21:36:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tenantfinder/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tenantfinder.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tenantfinder.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
